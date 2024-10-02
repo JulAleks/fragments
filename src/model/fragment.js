2086b24214d21f22b77f73e4941093a63dbd5afd
Julia Alekseev <yael.julia@gmail.com>
2024-10-01 20:58:15 -0400

    Added src/model and tests/unit files, updated .gitignore

---
+++ src/model/fragment.js
@@ -0,0 +1,157 @@
+//src/model/fragment.js
+// Use crypto.randomUUID() to create unique IDs, see:
+// https://nodejs.org/api/crypto.html#cryptorandomuuidoptions
+const { randomUUID } = require('crypto');
+// Use https://www.npmjs.com/package/content-type to create/parse Content-Type headers
+const contentType = require('content-type');
+
+// Functions for working with fragment metadata/data using our DB
+const {
+  readFragment,
+  writeFragment,
+  readFragmentData,
+  writeFragmentData,
+  listFragments,
+  deleteFragment,
+} = require('./data');
+
+class Fragment {
+  constructor({ id, ownerId, created, updated, type, size = 0 }) {
+    // Validating that all parameters were passed
+    if (!ownerId) {
+      throw new Error('ownerId is required');
+    }
+    if (!type) {
+      throw new Error('type is required');
+    }
+    if (typeof size !== 'number' || size < 0) {
+      throw new Error('size must be a non-negative number');
+    }
+    if (!Fragment.isSupportedType(type)) {
+      throw new Error(`Unsupported type: ${type}`);
+    }
+    this.id = id || randomUUID(); // Generate a new UUID if id is not provided
+    this.ownerId = ownerId; // Assign the passed id
+    this.created = created || new Date().toISOString(); // Default to current date if not provided
+    this.updated = updated || this.created; // Default to created date if updated is not provided
+    this.type = type; //Assign type
+    this.size = size; //Assign size
+  }
+
+  /**
+   * Get all fragments (id or full) for the given user
+   * @param {string} ownerId user's hashed email
+   * @param {boolean} expand whether to expand ids to full fragments
+   * @returns Promise<Array<Fragment>>
+   */
+  static async byUser(ownerId, expand = false) {
+    return await listFragments(ownerId, expand);
+  }
+
+  /**
+   * Gets a fragment for the user by the given id.
+   * @param {string} ownerId user's hashed email
+   * @param {string} id fragment's id
+   * @returns Promise<Fragment>
+   */
+  static async byId(ownerId, id) {
+    const fragment = await readFragment(ownerId, id);
+    if (!fragment) {
+      throw new Error(`Fragment with id ${id} not found`);
+    }
+    return fragment;
+  }
+
+  /**
+   * Delete the user's fragment data and metadata for the given id
+   * @param {string} ownerId user's hashed email
+   * @param {string} id fragment's id
+   * @returns Promise<void>
+   */
+  static async delete(ownerId, id) {
+    return await deleteFragment(ownerId, id);
+  }
+
+  /**
+   * Saves the current fragment to the database
+   * @returns Promise<void>
+   */
+  async save() {
+    this.updated = new Date().toISOString(); // Update the timestamp
+    return await writeFragment(this);
+  }
+
+  /**
+   * Gets the fragment's data from the database
+   * @returns Promise<Buffer>
+   */
+  async getData() {
+    return await readFragmentData(this.ownerId, this.id);
+  }
+
+  /**
+   * Set's the fragment's data in the database
+   * @param {Buffer} data
+   * @returns Promise<void>
+   */
+  async setData(data) {
+    if (!Buffer.isBuffer(data)) {
+      //If the data is not Buffer
+      throw new Error('Data must be a Buffer');
+    }
+    this.size = data.length; // Update the size with the length of the buffer
+    this.updated = new Date().toISOString(); // Update the timestamp
+    return await writeFragmentData(this.ownerId, this.id, data);
+  }
+
+  /**
+   * Returns the mime type (e.g., without encoding) for the fragment's type:
+   * "text/html; charset=utf-8" -> "text/html"
+   * @returns {string} fragment's mime type (without encoding)
+   */
+  get mimeType() {
+    const { type } = contentType.parse(this.type);
+    return type;
+  }
+
+  /**
+   * Returns true if this fragment is a text/* mime type
+   * @returns {boolean} true if fragment's type is text/*
+   */
+  get isText() {
+    return this.mimeType.startsWith('text/');
+  }
+
+  /**
+   * Returns the formats into which this fragment type can be converted
+   * @returns {Array<string>} list of supported mime types
+   */
+  get formats() {
+    // Supported content type to be added more next labs/assignments
+    const supportedFormats = {
+      'text/plain': ['text/plain'],
+    };
+    // Return types or empty array
+    return supportedFormats[this.mimeType] || [];
+  }
+
+  /**
+   * Returns true if we know how to work with this content type
+   * @param {string} value a Content-Type value (e.g., 'text/plain' or 'text/plain: charset=utf-8')
+   * @returns {boolean} true if we support this Content-Type (i.e., type/subtype)
+   */
+  static isSupportedType(value) {
+    // Supported content type to be added more next labs/assignments
+    const supportedTypes = {
+      'text/plain': ['text/plain'],
+    };
+
+    // Extract the MIME type from the value
+    const { type } = contentType.parse(value);
+
+    // Check if the extracted MIME type exists in the supportedTypes object
+    return Object.keys(supportedTypes).includes(type);
+  }
+}
+
+module.exports.Fragment = Fragment;
---
+++ tests/unit/memory.test.js
@@ -0,0 +1,148 @@
+const memoryIndex = require('../../src/model/data/memory/index');
+
+// Defining all the test fragments
+const fragment1 = {
+  ownerId: 'julia',
+  id: '001',
+  content: 'Cattle dogs for life!',
+};
+
+const fragment2 = {
+  ownerId: 'julia',
+  id: '002',
+  content: 'Bark, play, cuddle!',
+};
+
+const fragment3 = {
+  ownerId: 'julia',
+  id: '003',
+  content: 'Zoomies, treats, naps!',
+};
+
+const fragment4 = {
+  ownerId: 'julia',
+  id: '004',
+  content: 'Furry energy rockets!',
+};
+
+describe('GET /fragments', () => {
+  // Before each test save the fragments to the in memory db
+  beforeEach(async () => {
+    await memoryIndex.writeFragment(fragment1);
+    await memoryIndex.writeFragment(fragment2);
+    await memoryIndex.writeFragment(fragment3);
+  });
+
+  // Reading existing fragment
+  test('Read a fragment', async () => {
+    // Read the fragment from the memory db
+    const result = await memoryIndex.readFragment(fragment1.ownerId, fragment1.id);
+
+    // Expecting:
+    expect(result).toBeDefined();
+    expect(result.id).toBe(fragment1.id);
+    expect(result.ownerId).toBe(fragment1.ownerId);
+    expect(result.content).toBe(fragment1.content);
+  });
+
+  // Reading non-existing fragment
+  test('Read a non existing fragment', async () => {
+    // Read the fragment from the memory db
+    const result = await memoryIndex.readFragment('wrong_user', 'wrong_id');
+
+    // Expecting:
+    expect(result).toBeUndefined();
+  });
+
+  // Wrting data
+  describe('Write fragment data', () => {
+    test('Write a fragment', async () => {
+      await memoryIndex.writeFragment(fragment4);
+
+      const result = await memoryIndex.readFragment('julia', '004');
+      expect(result).toBeDefined();
+      expect(result.content).toBe(fragment4.content);
+    });
+
+    test('Write a fragment data buffer to memory', async () => {
+      // Creatign a new buffer
+      const buffer = Buffer.from('Donut worry, be happy!');
+      // Write buffer to memory db
+      await memoryIndex.writeFragmentData(fragment1.ownerId, fragment1.id, buffer);
+
+      // Read the fragment data to verify
+      const result = await memoryIndex.readFragmentData(fragment1.ownerId, fragment1.id);
+
+      // Expecting:
+      expect(result).toBeDefined();
+      expect(result).toEqual(buffer); // Expecting it to match the buffer
+    });
+
+    // Write a fragment data buffer to memory with missing user
+    test('Write a fragment data buffer to memory with missing user', async () => {
+      // Creating a new buffer
+      const buffer = Buffer.from('I will never be saved!');
+      const badUser = null;
+      const badID = '123';
+
+      // Expecting an error when writing a fragment with a missing userID
+      await expect(memoryIndex.writeFragmentData(badUser, badID, buffer)).rejects.toThrow(
+        `primaryKey and secondaryKey strings are required, got primaryKey=${badUser}, secondaryKey=${badID}`
+      );
+    });
+
+    // Write a fragment with null fragment id
+    test('Write a fragment with null fragment id', async () => {
+      // Creating a new buffer
+      const buffer = Buffer.from('I will never be saved!');
+      const badID = null;
+
+      // Expecting an error when writing a fragment with a missing userID
+      await expect(memoryIndex.writeFragmentData(fragment1.ownerId, badID, buffer)).rejects.toThrow(
+        `primaryKey and secondaryKey strings are required, got primaryKey=${fragment1.ownerId}, secondaryKey=${badID}`
+      );
+    });
+  });
+
+  // List all fragments
+  describe('Get all fragments for Julia', () => {
+    // Getting all fragments for "julia" without expand
+    test('Get all fragments for user without expand', async () => {
+      const result = await memoryIndex.listFragments('julia', false);
+
+      // Expecting:
+      expect(result).toBeDefined();
+      expect(Array.isArray(result)).toBe(true);
+      expect(result).toEqual(['001', '002', '003', '004']); // Expecting only IDs
+    });
+
+    // Getting all fragments for "julia" with expand
+    test('Get all fragments for user with expand', async () => {
+      const result = await memoryIndex.listFragments('julia', true);
+      // Expecting:
+      expect(result).toBeDefined();
+      expect(Array.isArray(result)).toBe(true);
+      expect(result).toEqual([fragment1, fragment2, fragment3, fragment4]); // Expecting full objects
+    });
+
+    // Getting all fragments for missing user
+    test('Getting all fragments for missing user', async () => {
+      await expect(memoryIndex.listFragments(null, true)).rejects.toThrow(
+        `primaryKey string is required, got primaryKey=${null}`
+      );
+    });
+  });
+
+  describe('Delete fragment', () => {
+    test('Delete fragment', async () => {
+      // Delete the fragment
+      await memoryIndex.deleteFragment(fragment1.ownerId, fragment1.id);
+
+      // Trying to read to see it was deleted
+      const deletedFragment = await memoryIndex.readFragmentData(fragment1.ownerId, fragment1.id);
+
+      // Expecting:
+      expect(deletedFragment).toBeUndefined(); // deletedFragment should be undefined
+    });
+  });
+});
