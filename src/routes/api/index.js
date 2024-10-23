31510950e2a98ed4a392b39f7085ed2e3cfa7a3e
Julia Alekseev <yael.julia@gmail.com>
2024-10-23 17:21:16 -0400

    updated names

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -9,13 +9,13 @@ const express = require('express');
 // Importing the `rawBody` middleware from `post.js`, which is used to parse raw binary data (e.g., Buffers).
 const rawBody = require('./post').rawBody;
 const postFragment = require('./post');
-const { getFragments, getFragmentById, getFragmentMetadataById } = require('./get');
+const { getFragments, getFragmentById, getFragmentDataById } = require('./get');
 
 // Create a router on which to mount our API endpoints
 const router = express.Router();
 
 // Routes - GET
-router.get('/fragments/:id/info', getFragmentMetadataById);
+router.get('/fragments/:id/info', getFragmentDataById);
 router.get('/fragments/:id', getFragmentById);
 router.get('/fragments', getFragments);
 
